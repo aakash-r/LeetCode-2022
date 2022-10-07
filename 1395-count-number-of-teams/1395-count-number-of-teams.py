ae8946ef1b99f634c5662483f2ed3affce811a48
aakash-r <60444584+aakash-r@users.noreply.github.com>
2022-10-07 15:20:13 -0700

    Time: 858 ms (81.29%), Space: 14.2 MB (18.35%) - LeetHub

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.py
@@ -0,0 +1,23 @@
+class Solution: 
+    def numTeams(self, rating: List[int]) -> int:
+        
+        
+        ans=0
+        for i,j in enumerate(rating):
+            ll,lh,rl,rh = 0,0,0,0
+            for k in range(0,i):
+                if rating[k]>j:
+                    lh+=1
+                elif rating[k]<j:
+                    ll+=1
+            for k in range(i+1,len(rating)):
+                if rating[k]>j:
+                    rh+=1
+                elif rating[k]<j:
+                    rl+=1
+            ans+= (ll*rh) + (lh*rl)
+            print(j,ll,lh,rl,rh)
+        return ans
+            
+            
+        
