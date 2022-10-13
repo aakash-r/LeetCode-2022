67101c6d18070e799b4a9eee5d3588fd656db462
aakash-r <60444584+aakash-r@users.noreply.github.com>
2022-10-12 18:40:46 -0700

    Time: 158 ms (38.10%), Space: 14 MB (54.15%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        total = 0 
+        i = 0
+        cur = []
+        res = []
+        
+        def comsum(i,total,cur):
+            if total == target:
+                res.append(cur)
+                return
+        
+            if total>target or i>=len(candidates):
+                return 
+            
+            
+            comsum(i+1,total,cur.copy())
+            cur.append(candidates[i])
+            comsum(i,total + candidates[i], cur.copy())
+           
+        comsum(0,0,[])
+        return res
