a0efbabd7aca61d501fd471f76a10f7469c0720f
aakash-r <60444584+aakash-r@users.noreply.github.com>
2022-10-14 15:06:24 -0700

    Time: 863 ms (17.72%), Space: 15.5 MB (35.98%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.py
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -1,28 +1,22 @@
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
-        dic = {}
-        for i in nums:
-            dic[i] = dic.get(i,0)+1
-        data = list(dic.items())
         d = defaultdict(list)
-        for i,j in data:
-            d[j].append(i)
-        maxc = max(d.keys())
-        possible = d[maxc]
-        ans = []
+        counts = {}
+        for i,ind in enumerate(nums):
+            if len(d[ind])>1:
+                d[ind].pop()
+            d[ind].append(i)
+            counts[ind]=counts.get(ind,0)+1
+        maxc = max(counts.values())
+        minl = len(nums)
+        if maxc == 1:
+            return 1
+        for i,j in counts.items():
+            if j==maxc:
+                minl=min(minl, d[i][1]-d[i][0]+1)
+        return minl
         
-        for cur in possible:
-            start = 0
-            end = len(nums)-1
-            while start<len(nums) and nums[start]!=cur:
-                start+=1
-
-            while end>0 and nums[end]!=cur:
-                end-=1
-
-            l = end-start+1
-            ans.append(l)
-        return min(ans)
+        
         
         
         
