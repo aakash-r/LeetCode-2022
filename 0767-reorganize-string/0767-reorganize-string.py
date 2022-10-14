92c0b50ee5195f68b7cf5743456d996e18d392b4
aakash-r <60444584+aakash-r@users.noreply.github.com>
2022-10-14 01:36:37 -0700

    Time: 41 ms (86.87%), Space: 13.8 MB (78.09%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -0,0 +1,31 @@
+from collections import Counter
+import heapq
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        count = Counter(s)
+        h = [[-c,charr] for charr,c in count.items()]
+        # print(h)
+        heapq.heapify(h)
+        # print(h)
+        ans = ''
+        prev = None
+        while h:
+            high = heapq.heappop(h)
+            ans+=high[1]
+            if prev is not None:
+                heapq.heappush(h, prev)
+            
+            if high[0]+1<0:
+                prev = [high[0]+1,high[1]]
+            else:
+                prev = None
+        if prev is not None:
+            if prev[0]==-1 and prev[1]!=ans[-1]:
+                ans+=prev[1]
+            else:
+                return ''
+            
+        return ans
+            
+        
+        
