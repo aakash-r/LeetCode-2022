aecfe24b10d9eae92e8b564fb56d74619699c010
aakash-r <60444584+aakash-r@users.noreply.github.com>
2022-10-14 17:50:10 -0700

    Time: 3111 ms (24.94%), Space: 13.8 MB (98.57%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -1,23 +1,25 @@
 class Solution:
+    res = 0
     def countArrangement(self, n: int) -> int:
         rem = list(range(1,n+1))
-        res = []
         r = rem.copy()
+        self.search([],r,1)
+        return self.res
         
-        def search(cur,rem,ind):
-            if len(rem)==0:
-                res.append(cur.copy())
+        
+        
+    def search(self,cur,rem,ind):
+        if len(rem)==0:
+            self.res+=1
             
-            for i in range(len(rem)):
+        for i in range(len(rem)):
 
-                num = rem[i]
-                if num%ind==0 or ind%num==0:
-                    cur.append(num)
+            num = rem[i]
+            if num%ind==0 or ind%num==0:
+                cur.append(num)
                     
-                    search(cur.copy(),rem[:i]+rem[i+1:], ind+1)
-                    cur.pop()
-                elif num%ind!=0 and ind%num!=0:
-                    continue
-        search([],r,1)
+                self.search(cur.copy(),rem[:i]+rem[i+1:], ind+1)
+                cur.pop()
+            elif num%ind!=0 and ind%num!=0:
+                continue
         
-        return len(res)
