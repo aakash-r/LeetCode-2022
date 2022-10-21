b3a21da35d02c5e9d7c9da7c7ec1c8795ca9185f
aakash-r <60444584+aakash-r@users.noreply.github.com>
2022-10-20 17:17:19 -0700

    Time: 2217 ms (7.17%), Space: 27.4 MB (44.08%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,11 +1,8 @@
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
-        
         maxvol = 0
-        
         s = 0
         e = len(heights)-1
-        curvol = 0
         
         while s<e:
             curvol = min(heights[s],heights[e])*(e-s)
